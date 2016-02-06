b155291341626f4a6a0e668ac9750ed14e519cd0
Jeff Stieler <jeff.m.stieler@gmail.com>
2016-02-05 16:23:45 -0800

    Set default form values, handle state changes, and display errors.

--- src/views/settings.js
+++ src/views/settings.js
@@ -1,11 +1,29 @@
 var React = require( 'react' );
-var GeneratedForm = require( 'react-form-generator' )({});
+var FG = require( 'react-form-generator' );
+var tools = FG.tools;
+var GeneratedForm = FG( {} );
 
 module.exports = React.createClass( {
 
+    getInitialState: function() {
+        return {
+            value: tools.evalDefaults( this.props.meta )
+        }
+    },
+
+    handleFormChanged: function ( newValue, change, fieldErrors ) {
+        this.setState( {
+            value: newValue,
+            errors: tools.merge( this.state.errors, fieldErrors )
+        } );
+    },
+
     render: function() {
         return (
-            <GeneratedForm meta={this.props.meta} />
+            <GeneratedForm meta={this.props.meta}
+                           value={this.state.value}
+                           errors={this.state.errors}
+                           onChange={this.handleFormChanged} />
         );
     }
 
